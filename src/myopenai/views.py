077528148b676a5bb99e9d9ac4f6cbf0107372f6
Igor <ihor.slodziak.ir.2022@lpnu.ua>
2023-05-11 12:54:05 +0300

    For recipe

--- src/myopenai/views.py
+++ src/myopenai/views.py
@@ -2,29 +2,38 @@ from django.shortcuts import render
 import openai
 import os
 
+api_key = os.environ.get('OPENAI_API_KEY')
 
-
-api_key=os.environ.get("OPENAI_API_KEY")
 
 def generate_response(request):
-     
     response_to_user = None
+    recipe_to_user = None
 
     if api_key is not None and request.method == "POST":
-        
         openai.api_key = api_key
 
         user_input = request.POST.get('user_input')
-        
+
         prompt = f"Я маю наступні продукти:{user_input}. Що я можу з цього приготувати?"
-   
+
         response = openai.Completion.create(
-            engine = "text-davinci-003", 
-            prompt = prompt, 
-            temperature = 0.5, 
-            max_tokens = 256)
+            engine="text-davinci-003",
+            prompt=prompt,
+            temperature=0.5,
+            max_tokens=256)
         print(response)
 
         response_to_user = response["choices"][0]["text"]
 
-    return render(request, 'ai_response.html', {'response': response_to_user})
+        prompt = f"Можеш сказати рецепт{response} цього?"
+
+        reciper = openai.Completion.create(
+            engine="text-davinci-003",
+            prompt=prompt,
+            temperature=0.5,
+            max_tokens=256)
+        print(response)
+
+        recipe_to_user = reciper["choices"][0]["text"]
+
+    return render(request, 'ai_response.html', {'response': response_to_user, 'recipe': recipe_to_user})
