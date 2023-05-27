588c2c9d1cbdfdf9999eec9aca19ba9bcb3cc144
Igor <ihor.slodziak.ir.2022@lpnu.ua>
2023-05-27 18:20:04 +0300

    add aboutUs

--- src/homepage/views.py
+++ src/homepage/views.py
@@ -5,4 +5,7 @@ from django.conf import settings
 def index(request):
     return render(request, "homepage/homepage.html")
 
+def about_us(request):
+    return render(request, "homepage/about_us.html")
+
 
